6c5e6a42889833e9646c061f6102f818b762adc4
vishwashegde21 <vishwas.hegde79@gmail.com>
2018-03-13 04:43:23 +0000

    first commit

---
+++ Sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int a,b,c;
+    printf("\nEnter a number:");
+    scanf("%d",&a);
+    printf("\nEnter another number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("\nThe sum of the two numbers is = %d",c);
+}
